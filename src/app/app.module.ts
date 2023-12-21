fe3f7bbb156f0ce98f38bc81113a54e2b770c2d9
Nihar Patel <nihar@Nihars-MBP.lan>
2023-12-21 10:42:23 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ-nwt7iD6KUzPl-ib_XsmnV67TRqm5pY",
+  authDomain: "girls-scout-cookies-tracker.firebaseapp.com",
+  projectId: "girls-scout-cookies-tracker",
+  storageBucket: "girls-scout-cookies-tracker.appspot.com",
+  messagingSenderId: "279209910836",
+  appId: "1:279209910836:web:ce8436ea613482f73de061",
+  measurementId: "G-YPD4PW87XT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
